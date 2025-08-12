eab509f772a9761aaa1b08a294ab1fe14fb7787c
ARSH KHAN <91189056+arshkhandesigns@users.noreply.github.com>
2025-08-13 00:20:41 +0530

    Add files via upload
    update till 13/08/2025

---
+++ pattern.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class pattern {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        /*
+         Output:
+          *
+          **
+          ***
+          ****
+         */
+
+         //Code:
+
+    //     for(int i =1; i<5; i++){
+    //     for(int j = 1; j<=i; j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println(" ");
+    // }
+
+    /*
+    Output: 
+    
+     *****
+     ****
+     ***
+     **
+     *
+     */
+
+     //Code:
+
+    //  for(int a=5; a>0; a--){
+    //     for(int b = 1; b<=a; b++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //  }
+
+    /*
+     
+    Output:
+    1
+    12
+    123
+    1234
+    12345
+    */
+     
+    //Code:
+
+    // for(int a=1; a<6; a++){
+    //     for(int b = 1; b<=a; b++){
+    //         System.out.print(b);
+    //     }
+    //     System.out.println();
+    //  }
+
+
+    /*
+     A
+     BC
+     DEF
+     GHIJ
+     */
+
+     //CODE: 
+
+//     char ch = 'A';
+//     for(int a=1; a<5; a++){
+//         for(int b = 1; b<=a; b++){
+//             System.out.print(ch);
+//             ch++;
+//         }
+//          System.out.println();
+// }
+
+    }
+}
