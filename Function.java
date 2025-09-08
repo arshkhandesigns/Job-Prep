0b9044f82065ff8f93ef6423e9b7fcd7260f1128
ARSH KHAN <91189056+arshkhandesigns@users.noreply.github.com>
2025-09-08 07:22:11 +0530

    Add files via upload
    updated till 08/09/2025

--- Function.java
+++ Function.java
@@ -74,12 +74,115 @@ public static int BCof(int n,int r){
     int BiCOF = factofn/(factofr*factofnr);
     return BiCOF;
 }
+
+//Check if n is prime number
+public static boolean isPrime(int n){
+    boolean isPrime =true;
+    if(n==2){
+        isPrime = true;
+    }
+    for(int i = 2; i<n ; i++){
+        if(n % i == 0){
+            isPrime = false;
+        }
+    }
+    return isPrime;
+}
+
+//print all prime in a range from 2 to n
+
+public static void PrimeRange(int n){
+    if(n==2){
+        System.out.print("2 ");
+    }
+    for(int i = 2; i < n; i++){
+        if(isPrime(i)){
+            System.out.print(i + " ");
+        }
+    }
+     System.out.println();
+}
+
+//Convert to Binary to Decimal
+public static void BitoDec(int n) {
+    int pow =0, dec =0;
+    while(n>0){
+        int ld = n%10;
+        dec = dec + (ld * (int)Math.pow(2, pow));
+        pow++;
+        n = n/10;
+    }   
+    System.out.println(dec);
+}
+
+public static void DectoBi(int n){
+    int rem = 0;
+    int bi = 0; 
+    int pow = 0;
+    while(n>0){
+        rem = n%2;
+        bi = bi + (rem * (int)Math.pow(10,pow));
+        pow++;
+        n = n/2;
+    }
+    System.out.println(bi);
+}
+
+public static void Avgthree(int a, int b, int c){
+    int sum = a+b+c;
+    int avg = sum/3;
+    System.out.println(avg);
+}
+
+public static boolean isEven(int n){
+    if(n%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+public static void isPalindrome(int n) {
+    int rev = 0;
+    int pal = n;
+    while(pal!=0){
+        int ld = pal%10;
+        rev = rev*10 + ld;
+        pal= pal/10;
+    }
+    if(n == rev){
+        System.out.println("Its a Palindrome Number.");
+    }
+    else{
+        System.out.println("It is not a Palindrome Number.");
+    }
+}
+
+public static void SumofDig(int n){
+    int sum = 0;
+    while(n>0){
+        int ld = n%10;
+        sum = sum + ld;
+        n =n/10;
+    }
+    System.out.println(sum);
+}
+
 public static void main(String[] args) {
     // tableof();
     Scanner s = new Scanner(System.in);
     int n  = s.nextInt();
-    int r = s.nextInt();
-    System.out.println(factotrialof(n));
-    System.out.println(BCof(n, r));
+    // int r = s.nextInt();
+    // System.out.println(factotrialof(n));
+    // System.out.println(BCof(n, r));
+    // System.out.println(isPrime(n));
+    // PrimeRange(n);
+    // BitoDec(n);
+    // DectoBi(n);
+    // Avgthree(2,5,6);
+    // System.out.println(isEven(n));
+    // isPalindrome(n);
+    // SumofDig(n);
 }
 }
